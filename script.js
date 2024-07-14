f49a4c46816b753e95bd217ccb3b53862e9e221c
Adithya <nkvadithya2@gmail.com>
2024-07-14 11:41:08 +0530

    updated script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ async function checkWeather(city){
     
     if(response.status==404){
         document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
     }
     else
     {
